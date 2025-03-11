1ef6c3acce4e331c8134687b08746fe7341a7cc9
sael-sellesfrancesconi <sael.sellesfrancesconi@outlook.com>
2025-03-11 16:40:33 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('signupForm').addEventListener('submit', function(event) {
-   event.preventDefault();
-   alert('Formulaire d\'inscription soumis!');
-});
-
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-   event.preventDefault();
-   alert('Formulaire de connexion soumis!');
-});
