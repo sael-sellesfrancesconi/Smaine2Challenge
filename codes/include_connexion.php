69c7a2da69a108f4866996346a7ce39262fdb91f
MarvinFm <marvin.fm54@gmail.com>
2025-03-12 10:14:43 +0100

    Amelioration page principale

--- codes/include_connexion.php
+++ codes/include_connexion.php
@@ -1,15 +1,17 @@
 
-        <form method="post" action="./codes/action_traitement_connexion.php">
-                    
-            <h2> Connexion </h2>
+        <form id="form" method="post" action="./codes/action_traitement_connexion.php">
             
+             
+            <h2> Connexion </h2>
+            <br>  
             <h3> Identifiant </h3>
             <input type="text" name="id" required>
-                    
+            <br>      
+            <br>  
             <h3>Mot de passe</h3>
             <div class="barre">
                 <input type="password" name="mdp1" id="mdp1" required>
-                <button type="button" id="aff1" onclick="togglePassword('mdp1')">Aff</button>
+                <button type="button" class="aff" id="aff1" onclick="togglePassword('mdp1')">Aff</button>
             </div>
                     
             <input type="hidden" name="token" value="<?= htmlspecialchars($_SESSION['token'], ENT_QUOTES, 'UTF-8'); ?>">
--- codes/include_inscription.php
+++ codes/include_inscription.php
@@ -1,22 +1,24 @@
 
-        <form method="post" action="./codes/action_traitement_inscription.php">
+        <form id="form" method="post" action="./codes/action_traitement_inscription.php">
             
             <h2> Créer un compte </h2>
+            <br>
             <h3> Identifiant </h3>
             <input type="text" name="id" required>
-                    
+            <br>  
+            <br>  
             <h3> Mot de passe </h3>
             <div class="barre">
                 <input type="password" name="mdp2" id="mdp2" required>
                 <button type="button" class="aff" id="aff2" onclick="togglePassword('mdp2')">Aff</button>
             </div>
-                    
+            <br>  
             <h3> Confirmez </h3>
             <div class="barre">
                 <input type="password" name="mdp3" id="mdp3">
                 <button type="button" class="aff" id="aff3" onclick="togglePassword('mdp3')">Aff</button>
             </div>
-
+            <br>  
             <h3> Je suis: </h3>
             <select id="role-select">
                 <option value="">Sélectionnez</option>
@@ -28,6 +30,7 @@
                 <button id="inscription-button">Inscription</button>
                 <button id="connexion-button">Connexion</button>
             </div>
+            <br>
             <br>
             <input type="hidden" name="token" value="<?= htmlspecialchars($_SESSION['token'], ENT_QUOTES, 'UTF-8'); ?>">
             <input type="submit" name="creer" value="Créer le compte">
